58834a128375daf5420652b09d577e3bb95ec755
Barbariansyah <43785141+Barbariansyah@users.noreply.github.com>
2018-11-24 16:44:07 +0700

    Add files via upload

--- player.pl
+++ player.pl
@@ -15,14 +15,14 @@ randomCoordinate(X, Y):-
     X is A, Y is B.
 
 
-init_Player:-
-    initHealth(Health),
-    initArmor(Armor),
-    initWeapon(Weapon),
-    initInventory(Inventory),
-    randomCoordinate(X, Y),
-    initAmmo(Ammo),
-    asserta(player(X,Y,Health,Armor,Weapon,Inventory,Ammo)), !.
+    init_Player:-
+        initHealth(Health),
+        initArmor(Armor),
+        initWeapon(Weapon),
+        initInventory(Inventory),
+        randomCoordinate(X, Y),
+        initAmmo(Ammo),
+        asserta(player(X,Y,Health,Armor,Weapon,Inventory,Ammo)), !.
 
 
 
@@ -163,11 +163,6 @@ step_up:-
     player(X,CurrentY,Health,Armor,Weapon,Inventory,Ammo),
     CurrentY > 0,
     Y is CurrentY-1,
-    deadzone_timer(T),
-    Tn is T-1, 
-    retract(deadzone_timer(T)), asserta(deadzone_timer(Tn)),
-    (Tn == 0, write('DEADZONE IS SHRINKING! BE CAREFUL.'), nl;
-     write(Tn), write(' more tick to deadzone shrink. watch your move.'),nl),
     retract(player(X,CurrentY,Health,Armor,Weapon,Inventory,Ammo)),
     asserta(player(X,Y,Health,Armor,Weapon,Inventory,Ammo)).
 
@@ -175,11 +170,6 @@ step_down:-
     player(X,CurrentY,Health,Armor,Weapon,Inventory,Ammo),
     CurrentY < 20,
     Y is CurrentY+1,
-    deadzone_timer(T),
-    Tn is T-1, 
-    retract(deadzone_timer(T)), asserta(deadzone_timer(Tn)),
-    (Tn == 0, write('DEADZONE IS SHRINKING! BE CAREFUL.'), nl;
-     write(Tn), write(' more tick to deadzone shrink. watch your move.'),nl),
     retract(player(X,CurrentY,Health,Armor,Weapon,Inventory,Ammo)),
     asserta(player(X,Y,Health,Armor,Weapon,Inventory,Ammo)).
 
@@ -187,11 +177,6 @@ step_left:-
     player(CurrentX,Y,Health,Armor,Weapon,Inventory,Ammo),
     CurrentX > 0,
     X is CurrentX-1,
-    deadzone_timer(T),
-    Tn is T-1, 
-    retract(deadzone_timer(T)), asserta(deadzone_timer(Tn)),
-    (Tn == 0, write('DEADZONE IS SHRINKING! BE CAREFUL.'), nl;
-     write(Tn), write(' more tick to deadzone shrink. watch your move.'),nl),
     retract(player(CurrentX,Y,Health,Armor,Weapon,Inventory,Ammo)),
     asserta(player(X,Y,Health,Armor,Weapon,Inventory,Ammo)).
 
@@ -199,11 +184,6 @@ step_right:-
     player(CurrentX,Y,Health,Armor,Weapon,Inventory,Ammo),
     CurrentX < 20,
     X is CurrentX+1,
-    deadzone_timer(T),
-    Tn is T-1, 
-    retract(deadzone_timer(T)), asserta(deadzone_timer(Tn)),
-    (Tn == 0, write('DEADZONE IS SHRINKING! BE CAREFUL.'), nl;
-     write(Tn), write(' more tick to deadzone shrink. watch your move.'),nl),
     retract(player(CurrentX,Y,Health,Armor,Weapon,Inventory,Ammo)),
     asserta(player(X,Y,Health,Armor,Weapon,Inventory,Ammo)).
 
@@ -237,11 +217,15 @@ decrease_HealthE(Id, Amount):-
 check_enemy_exist :-
 	player(X,Y,_,_,_,_,_),
 	is_enemy_exist(X, Y),
-	write('There\'s enemy in your sight'), nl, !.
+	write('There\'s enemy in your sigh'), nl, !.
 
 is_enemy_exist(X, Y) :-
-	enemy(_, A, B, _),
+	enemy(_, A, B, Health),
 	A =:= X, B =:= Y, !.
+
+is_enemy_all_dead(X,Y) :- 
+	enemy(_,_,_,Health),
+	Health=0,!.
 
 
 /*nearby enemy*/
@@ -285,43 +269,42 @@ generate_random_move(N) :- random_move(N), M is N-1, generate_random_move(M).
 random_move(Id) :-
 	random(1, 6, N),
 	step(Id, N), !.
-
 random_move(_) :- !.
 
-step(Id, 1) :-
+  step(Id, 1) :-
   	stepup(Id), !.
-step(Id, 2) :-
+  step(Id, 2) :-
   	stepdown(Id), !.
-step(Id, 3) :-
+  step(Id, 3) :-
   	stepleft(Id), !.
-step(Id, 4) :-
+  step(Id, 4) :-
   	stepright(Id), !.
-step(Id, 5) :- !.
+  step(Id, 5) :- !.
 
-stepup(Id):-
+  stepup(Id):-
   	enemy(Id, X, CurrentY, Health),
   	CurrentY > 0,
   	NewY is CurrentY-1,
   	retract(enemy(Id, X, CurrentY, Health)),
   	asserta(enemy(Id, X, NewY, Health)).
 
-stepdown(Id):-
+  stepdown(Id):-
   	enemy(Id, X, CurrentY, Health),
-  	CurrentY < 19,
+  	CurrentY < 20,
   	NewY is CurrentY+1,
   	retract(enemy(Id, X, CurrentY, Health)),
   	asserta(enemy(Id, X, NewY, Health)).
 
-stepleft(Id):-
+  stepleft(Id):-
   	enemy(Id, CurrentX, Y, Health),
   	CurrentX > 0,
   	NewX is CurrentX-1,
   	retract(enemy(Id, CurrentX, Y, Health)),
   	asserta(enemy(Id, NewX, Y, Health)).
 
-stepright(Id):-
+  stepright(Id):-
   	enemy(Id, CurrentX, Y, Health),
-  	CurrentX < 19,
+  	CurrentX < 20,
   	NewX is CurrentX+1,
   	retract(enemy(Id, CurrentX, Y, Health)),
   	asserta(enemy(Id, NewX, Y, Health)).
